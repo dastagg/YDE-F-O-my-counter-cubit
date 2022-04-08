727571b39c451ab807c893224a649ee8e55d4b51
dastagg:opus5 <dastagg@stagglinux.com>
2022-04-08 08:33:15 -0400

    Update cubit counter modules.

--- lib/cubits/counter/counter_state.dart
+++ lib/cubits/counter/counter_state.dart
@@ -1,10 +1,26 @@
 part of 'counter_cubit.dart';
 
-abstract class CounterState extends Equatable {
-  const CounterState();
+class CounterState extends Equatable {
+  final int counter;
+  CounterState({
+    required this.counter,
+  });
+
+  factory CounterState.initial() {
+    return CounterState(counter: 0);
+  }
 
   @override
-  List<Object> get props => [];
-}
+  List<Object> get props => [counter];
 
-class CounterInitial extends CounterState {}
+  @override
+  String toString() => 'CounterState(counter: $counter)';
+
+  CounterState copyWith({
+    int? counter,
+  }) {
+    return CounterState(
+      counter: counter ?? this.counter,
+    );
+  }
+}
